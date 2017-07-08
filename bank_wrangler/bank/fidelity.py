c43ea15b8f0f8e11fd4ed39bb1ea5ebe0892c578
Trevor Merrifield <trevorm42@gmail.com>
2017-07-08 16:23:09 -0400

    Add balance correcting transactions for fidelity

--- bank_wrangler/bank/fidelity.py
+++ bank_wrangler/bank/fidelity.py
@@ -28,10 +28,16 @@ Other ways we could fetch transactions from Fidelity:
 import csv
 import shutil
 import tempfile
+import json
 from decimal import Decimal
 from bank_wrangler.config import ConfigField
 from bank_wrangler import schema
-from bank_wrangler.bank.common import FirefoxDownloadDriver, fidelity_login
+from bank_wrangler.bank.common import (
+    FirefoxDownloadDriver,
+    fidelity_login,
+    add_balance_correcting_transaction,
+    assert_issubset,
+)
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -49,6 +55,28 @@ def empty_config():
     ]
 
 
+def _account_balances(driver):
+    """Grab account balances from the Accounts FinApp."""
+    driver.switch_to.frame(driver.find_element_by_id('content'))
+    driver.switch_to.frame(driver.find_element_by_name('content'))
+    driver.switch_to.frame(
+        driver.find_element_by_xpath('//iframe[@title="Accounts FinApp"]'))
+
+    accounts_upper = driver.find_elements_by_class_name('account-text')
+    accounts_lower = driver.find_elements_by_class_name('account-lower-text')
+    accounts = []
+    for upper, lower in zip(accounts_upper, accounts_lower):
+        lowerleft = lower.text.split('-')[0].strip()
+        accounts.append('{} - {}'.format(upper.text, lowerleft))
+
+    amounts = driver.find_elements_by_class_name('amount-text')
+    replacements = {ord(ch): '' for ch in ' $,'}
+    amounts_text = [amt.text.splitlines()[0].translate(replacements)
+                    for amt in amounts]
+
+    return dict(zip(accounts, amounts_text))
+
+
 def _download(config, tempdir):
     username, password = config
 
@@ -58,23 +86,17 @@ def _download(config, tempdir):
     driver.get('https://scs.fidelity.com/customeronly/fullview.shtml')
     fidelity_login(driver, username.value, password.value)
 
-    # Navigate through the jungle of frames.
     driver.implicitly_wait(30)
-    driver.switch_to.frame(driver.find_element_by_id('content'))
-    driver.switch_to.frame(driver.find_element_by_name('content'))
 
-    # Wait until this arbitrary iframe loads before clicking through menus.
-    # This adds a conservative delay that fixes an issue where sometimes
-    # the upcoming mouse move onto the menu doesn't do anything.
-    driver.switch_to.frame(
-        driver.find_element_by_xpath('//iframe[@title="Net Worth Performance FinApp"]'))
-    driver.find_element_by_id('todays-networth')
+    # Grab account balances, which also introduces a helpful delay so
+    # we don't mouse over the "My Accounts" menu too soon.
+    balances = _account_balances(driver)
 
     driver.switch_to_default_content()
     driver.switch_to.frame(driver.find_element_by_id('content'))
     driver.switch_to.frame(driver.find_element_by_name('content'))
 
-    # Click My Account > Transactions.
+    # Click My Accounts > Transactions.
     menu_elem = driver.find_element_by_id('leve2Menu2')
     ActionChains(driver).move_to_element(menu_elem).perform()
     wait = WebDriverWait(driver, 5)
@@ -97,20 +119,27 @@ def _download(config, tempdir):
     csv_path = driver.grab_download('ExportData.csv', timeout_seconds=30)
     driver.quit()
 
-    return csv_path
+    return csv_path, balances
 
 
 def fetch(config, fileobj):
     with tempfile.TemporaryDirectory() as tempdir:
-        csv_path = _download(config, tempdir)
+        csv_path, balances = _download(config, tempdir)
+        json.dump(balances, fileobj)
+        fileobj.write('\n')
         with open(csv_path, 'r') as csv_file:
             shutil.copyfileobj(csv_file, fileobj)
 
 
 def transactions(fileobj):
     result = schema.TransactionModel(schema.COLUMNS)
-    lines = list(csv.reader(fileobj))[1:]
-    for _, date, description, _, _, _, signed_amount, _, _, _, account, _ in lines:
+    jsonbalances, _csv_header, *csvlines = iter(fileobj)
+    balances_by_account = {
+        account: Decimal(balance)
+        for account, balance
+        in json.loads(jsonbalances).items()
+    }
+    for _, date, description, _, _, _, signed_amount, _, _, _, account, _ in csv.reader(csvlines):
         month, day, year = map(int, date.split('/'))
         frm, to = 'Universe', account
         amount = Decimal(signed_amount.replace(',', ''))
@@ -125,9 +154,14 @@ def transactions(fileobj):
             schema.String(description),
             schema.Dollars(amount)
         )
+    for account, balance in balances_by_account.items():
+        add_balance_correcting_transaction(name(), account, balance, result)
     return result
 
 
 def accounts(fileobj):
-    lines = list(csv.reader(fileobj))[1:]
-    return {account for *_, account, _ in lines}
+    jsonbalances, _csv_header, *csvlines = iter(fileobj)
+    all_accounts = json.loads(jsonbalances).keys()
+    tabulated_accounts = {account for *_, account, _ in csv.reader(csvlines)}
+    assert_issubset(tabulated_accounts, all_accounts)
+    return all_accounts
