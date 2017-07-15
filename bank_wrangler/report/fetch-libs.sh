729ef0302afeeade6ab7e74f0414d546f1bc04c7
Trevor Merrifield <trevorm42@gmail.com>
2017-07-14 22:13:13 -0400

    Add pure.css to report/libs

--- bank_wrangler/report/fetch-libs.sh
+++ bank_wrangler/report/fetch-libs.sh
@@ -2,3 +2,4 @@
 wget -P libs https://cdn.datatables.net/v/dt/jq-2.2.4/dt-1.10.15/datatables.min.css
 wget -P libs https://cdn.datatables.net/v/dt/jq-2.2.4/dt-1.10.15/datatables.min.js
 wget -P libs https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js
+wget -P libs https://unpkg.com/purecss@1.0.0/build/pure-min.css
